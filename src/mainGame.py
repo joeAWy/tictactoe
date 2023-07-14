99ee920a0c5a40e6dcc445e9ad7faef7a4685896
joeAWy <josephawy@aol.com>
2023-07-13 22:30:15 -0400

    Add game over and score Fixes #1

--- src/mainGame.py
+++ src/mainGame.py
@@ -12,6 +12,8 @@ from pygame.locals import (
     QUIT
 )
 
+PLAYER1_SCORE = 0
+PLAYER2_SCORE = 0
 
 class MainGame(Scene):
     def __init__(self, display):
@@ -23,10 +25,14 @@ class MainGame(Scene):
             for y in range(self.columns):
                 self.gameBoard.add(Tile(x, y))
         # pygame.font.render(text, antialias, color background=None) -> Surface
-        self.quitButton = self.font.render("Quit Game", True, Color(0,0,0))
+        self.quitButton = self.font.render("Quit Game?", True, Color(0,0,0))
         # pygame.Surface.get_rect(**kwargs) -> Rect : keyword args can set rect position
         self.quitButtonRect = self.quitButton.get_rect(center=(self.display.get_width() - 100, self.display.get_height() - 50))
         self.nextScene = GameOver(self.display)
+        self.player1Score = self.font.render("Player 1:   " + str(PLAYER1_SCORE), True, Color(0,0,0))
+        self.player2Score = self.font.render("Player 2:   " + str(PLAYER2_SCORE), True, Color(0,0,0))
+        self.player1ScoreRect = self.player1Score.get_rect(center=(self.display.get_width() - 150, 50))
+        self.player2ScoreRect = self.player2Score.get_rect(center=(self.display.get_width() - 150, 100))
 
     def handle_events(self, event):
         if event.type == MOUSEBUTTONDOWN:
@@ -47,32 +53,56 @@ class MainGame(Scene):
     def draw(self):  # todo make sure last draw occurs before changing to game-over screen
         self.display.fill((255,255,255))
         self.display.blit(self.quitButton, self.quitButtonRect)
+        self.display.blit(self.player1Score, self.player1ScoreRect)
+        self.display.blit(self.player2Score, self.player2ScoreRect)
         for tile in self.gameBoard: # todo test and see if i can replace loop with spritegroup.draw()
             self.display.blit(tile.image, tile.imageRect)
         update()
         
 
     def check_column_match(self):
+        global PLAYER1_SCORE
+        global PLAYER2_SCORE
         for x in range(self.columns):
             column = [y.imageIndex for y in self.gameBoard if y.columnNumber == x]
             if column[0] == column[1] and column[1] == column[2] and column[0] != EMPTY_TILE:
+                if column[0] == CROSS_TILE:
+                    PLAYER1_SCORE += 1
+                else:
+                    PLAYER2_SCORE += 1
                 return True       
         return False
     
     def check_row_match(self):
+        global PLAYER1_SCORE
+        global PLAYER2_SCORE
         for x in range(self.rows):
             row = [y.imageIndex for y in self.gameBoard if y.rowNumber == x]
             if row[0] == row[1] and row[1] == row[2] and row[0] != EMPTY_TILE:
+                if row[0] == CROSS_TILE:
+                    PLAYER1_SCORE += 1
+                else:
+                    PLAYER2_SCORE += 1
                 return True
         return False
     
     def check_diagonal_match(self):
+        global PLAYER1_SCORE
+        global PLAYER2_SCORE
         diagonal1 = [d1.imageIndex for d1 in self.gameBoard if d1.rowNumber == d1.columnNumber]
         if diagonal1[0] == diagonal1[1] and diagonal1[1] == diagonal1[2] and diagonal1[0] != EMPTY_TILE:
+            if diagonal1[0] == CROSS_TILE:
+                PLAYER1_SCORE += 1
+            else:
+                PLAYER2_SCORE += 1
             return True
                 
         diagonal2 = [d2.imageIndex for d2 in self.gameBoard if d2.rowNumber + d2.columnNumber == 2]
         if diagonal2[0] == diagonal2[1] and diagonal2[1] == diagonal2[2] and diagonal2[0] != EMPTY_TILE:
+            if diagonal2[0] == CROSS_TILE:
+                PLAYER1_SCORE += 1
+            else:
+                PLAYER2_SCORE += 1
             return True
         return False
                 
